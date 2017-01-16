e56123ff80453c626bf65f6b6a3f4df922c14db3
Anush <movsesyan.anush@gmail.com>
2017-01-17 01:16:21 +0400

    Generator changes

---
+++ PassGen/PassGen/Generator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassGen
+{
+    class PassGen
+    {
+        const string numb = "0123456789";
+        const string lettL = "abcdefghijklmnopqrstuvwxyz";
+        const string lettU = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+        private char[] pin;
+        private string letters = String.Empty;
+        private string[] lettersarray = { numb, lettL, lettU };
+
+        public PassGen(int length, params int[] condition)
+        {
+            if (length >= 8)
+            {
+                pin = new char[length];
+            }
+            else
+                throw new ArgumentOutOfRangeException("Password can not contain less than 8 members");
+
+        }
+    }
+}
