6579635dd53a1a218a602e72777b585625cdc1ec
Anush <movsesyan.anush@gmail.com>
2017-01-17 01:13:39 +0400

    Test

---
+++ PassGen/PassGen/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PassGen Pw = new PassGen(0, 1, 2, 3);
+            Console.WriteLine(Pw);
+        }
+    }
+}
