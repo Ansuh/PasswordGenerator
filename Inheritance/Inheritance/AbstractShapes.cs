e54345220ceed28612af0ace3fdd08ad38304742
Anush <movsesyan.anush@gmail.com>
2017-01-21 09:11:35 +0400

    Not complete

---
+++ Inheritance/Inheritance/AbstractShapes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance3DShapes
+{
+    public abstract class AbstractShapes
+    {
+        public abstract Point point { get; set; }
+        public abstract float Volume();
+        public abstract float Arema();
+     
+
+    }
+}
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance3DShapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cone c = new Cone();
+            Console.WriteLine();
+
+
+
+
+
+        }
+    }
+}
