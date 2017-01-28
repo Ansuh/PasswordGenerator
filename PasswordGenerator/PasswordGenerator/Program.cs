bc56e2c6d87936a7364d01bfd82ea4e7e5862e0e
Anush <movsesyan.anush@gmail.com>
2017-01-28 12:16:55 +0400

    Password generator cod

--- PasswordGenerator/PasswordGenerator/Program.cs
+++ PasswordGenerator/PasswordGenerator/Program.cs
@@ -8,8 +8,36 @@ namespace PasswordGenerator
 {
     class Program
     {
+        private const string capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string smallLetters = "abcdefghijklmnopqrstuvwxyz";
+        private const string digits = "0123456789";
+        private const string specChar = "!@#$%^&*~`?/";
+        private const string allChars = capitalLetters + smallLetters + specChar;
+        private static Random rnd = new Random();
+        
+
         static void Main(string[] args)
-        {abc
+        {
+            StringBuilder password = new StringBuilder();
+
+            for (int i = 1; i <= 2; i++)
+            {
+                char capitalLetter = GenerateChar(capitalLetters);
+                insertAtRandomPositions(password, capitalLetter);
+            }
+            for (int i = 1; i <= 2; i++)
+            {
+                char smallLetter = GenerateChar(smallLetters);
+                insertAtRandomPositions(password, smallLetter);
+            }
+            for (int i = 1; i <= 2; i++)
+            {
+                char capitalLetter = GenerateChar(capitalLetters);
+                insertAtRandomPositions(password, capitalLetter);
+            }
+
+
+
         }
     }
 }
