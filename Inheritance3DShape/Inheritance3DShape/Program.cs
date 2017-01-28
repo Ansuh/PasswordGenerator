666b38f9b9ae3d387c8cda68a7b9c4bf1ab3cdcd
Anush <movsesyan.anush@gmail.com>
2017-01-28 11:57:27 +0400

    Commit 1

---
+++ Inheritance3DShape/Inheritance3DShape/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance3DShape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cone c = new Cone();
+            Console.WriteLine();
+
+        }
+    }
+}
